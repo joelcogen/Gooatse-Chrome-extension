8c73bb6b9cccfc21d041ddfd75597e6f0b48b432
Joel Cogen <joel@joelcogen.com>
2011-02-17 17:57:40 +0100

    Added modified logo on results page (1.2)

--- gooatse.js
+++ gooatse.js
@@ -1,11 +1,16 @@
 var imgURL = chrome.extension.getURL("google-goatse.gif");
+var imgResultsURL = chrome.extension.getURL("google-goatse-results.png");
+
 var hplogo = document.getElementById("hplogo");
 if(hplogo==null) hplogo = document.getElementById("logo");
 
-if(hplogo.src == undefined) {
-	// Div background, eg google.be
-	hplogo.style.background = "url("+imgURL+") no-repeat";
-}else{
+if(hplogo.src != undefined) {
 	// Image, google.com
 	hplogo.src = imgURL;
+}else if(hplogo.href != undefined) {
+	// Search results page
+	hplogo.children[0].src = imgResultsURL;
+}else{
+	// Div background, eg google.be
+	hplogo.style.background = "url("+imgURL+") no-repeat";
 }
